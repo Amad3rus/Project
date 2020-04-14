5e144ff7cce0daf8a1adb84db1960db08f453b3f
Roberto <roberto.rosa7@gmail.com>
2020-04-14 17:20:12 -0300

    reajuste no layout de contato

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -4,7 +4,7 @@ export default class AppController {
     constructor(){
         this.fetchIds();
         this.initEvents();
-
+        this.fetchContacts();
         this.config = {
             animate:'animated',
             fadeinleft:'fadeInLeft',
@@ -21,6 +21,8 @@ export default class AppController {
             this.el[Format.formatToCamelCase(element.id)] = element;
         });
         this.el['menuSetinha'].checked = true;
+
+        console.log(this.el);
     }
 
     initEvents(){
@@ -49,5 +51,47 @@ export default class AppController {
                 this.el['menuSetinha'].checked = false;
             }
         }
+    }
+
+    fetchContacts(){
+        const contacts = [
+            {
+                name:'Kakashi',
+                message:'olá',
+                time: new Date().getTime()
+            },
+            {
+                name:'Maria',
+                message:'já estou aqui',
+                time: new Date().getTime()
+            },
+            {
+                name:'João',
+                message:'Não foi?',
+                time: new Date().getTime()
+            },
+            {
+                name:'Vinícius',
+                message:'tudo bem?',
+                time: new Date().getTime()
+            }
+        ]
+        contacts.forEach((value, index) => {
+            let li = document.createElement('li');
+            li.id = 'list-' + index;
+            li.innerHTML += `
+                <button type="button">
+                    <div class="img">
+                        <i class="large material-icons">person</i>
+                    </div>
+                    <div class="text">
+                        <span class="name">${value.name}</span>
+                        <span class="message">${value.message}</span>
+                        <span class="time">${value.time}</span>
+                    </div>
+                </button>
+            `
+            this.el['listContact'].appendChild(li);
+        });
     }
 }
